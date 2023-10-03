187616a25cec6f16199e55ba4452a1bf2e44e683
talha-amin <talha.amin@cubixlabs.com>
2023-10-04 02:16:17 +0500

    updated social login & modal

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.js"],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "firebase/auth",
+        "firebase/firestore",
+        "firebase/app",
+        "firebase/analytics",
+      ],
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
   },
 });
